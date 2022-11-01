59d83c487e5ba41f90567f6255053cedb20e3b70
MinjiKim <minji@MinjiKimui-MacBookPro.local>
2022-11-01 21:14:43 +0900

    Add README

---
+++ README.md
@@ -0,0 +1,41 @@
+<h1 align="center">Welcome to date-picker 👋</h1>
+<p>
+  <a href="https://www.npmjs.com/package/date-picker" target="_blank">
+    <img alt="Version" src="https://img.shields.io/npm/v/date-picker.svg">
+  </a>
+  <a href="#" target="_blank">
+    <img alt="License: ISC" src="https://img.shields.io/badge/License-ISC-yellow.svg" />
+  </a>
+</p>
+
+> Date Picker implemented in JavaScript. CSS variables are used to adjust font size whenever the width of the calendar changes.
+
+## Install
+
+```sh
+npm install
+```
+
+## Usage
+
+```sh
+run using the Visual Code Live Server extension
+```
+
+## Run tests
+
+```sh
+Coming Soon
+```
+
+## Author
+
+👤 **Minji Kim**
+
+* Github: [@minji99710](https://github.com/minji99710)
+* LinkedIn: [@kimbision](https://linkedin.com/in/kimbision)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
--- components/calendar.js
+++ components/calendar.js
@@ -1,92 +1,95 @@
 const get = (target) => {
-  return document.querySelector(target)
-}
+    return document.querySelector(target);
+};
 
-export default function Calendar () {
-  const $selectedMonth = get('.selected-month')
-  const $selectedYear = get('.selected-year')
-  const $days = get('.days')
-  const months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December'
-  ]
+export default function Calendar() {
+    const $selectedMonth = get(".selected-month");
+    const $selectedYear = get(".selected-year");
+    const $days = get(".days");
+    const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
 
-  const date = new Date()
+    const date = new Date();
 
-  const renderCalendar = () => {
-    date.setDate(1) // 찾아보기
-    const month = date.getMonth()
-    const year = date.getFullYear()
-    const lastDay = new Date(year, month + 1, 0).getDate()
-    const prevLastDay = new Date(year, month, 0).getDate()
-    const firstdayIndex = date.getDay()
-    const lastDayIndex = new Date(year, month + 1, 0).getDay() // 현재 달 마지막 요일
-    const nextDays = 7 - lastDayIndex - 1
+    const renderCalendar = () => {
+        date.setDate(1);
+        const month = date.getMonth();
+        const year = date.getFullYear();
+        const lastDay = new Date(year, month + 1, 0).getDate();
+        const prevLastDay = new Date(year, month, 0).getDate();
+        const firstdayIndex = date.getDay();
+        const lastDayIndex = new Date(year, month + 1, 0).getDay(); // 현재 달 마지막 요일
+        const nextDays = 7 - lastDayIndex - 1;
 
-    $selectedMonth.textContent = months[month]
-    $selectedYear.textContent = year
+        $selectedMonth.textContent = months[month];
+        $selectedYear.textContent = year;
 
-    const dayNames = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
+        const dayNames = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 
-    // 달력에 요일 표시
-    const createDayNames = () => {
-      const $dayNamesFragment = document.createDocumentFragment()
-      dayNames.forEach((day, idx) => {
-        const $daystr = document.createElement('div')
-        $daystr.classList.add('day', 'daystr')
-        $daystr.textContent = dayNames[idx]
-        $dayNamesFragment.appendChild($daystr)
-      })
-      $days.appendChild($dayNamesFragment)
-    }
+        // 달력에 요일 표시
+        const createDayNames = () => {
+            const $dayNamesFragment = document.createDocumentFragment();
+            dayNames.forEach((day, idx) => {
+                const $daystr = document.createElement("div");
+                $daystr.classList.add("day", "daystr");
+                $daystr.textContent = dayNames[idx];
+                $dayNamesFragment.appendChild($daystr);
+            });
+            $days.appendChild($dayNamesFragment);
+        };
 
-    // 달력에 날짜(숫자) 표시
-    const showDates = () => {
-      $days.innerHTML = ''
-      createDayNames()
-      let days = ''
-      // 이전 달 날짜 표시
-      for (let i = firstdayIndex; i > 0; i--) {
-        days += `<div class='day prev-date'>${prevLastDay - i + 1}</div>`
-      }
+        // 달력에 날짜(숫자) 표시
+        const showDates = () => {
+            $days.innerHTML = "";
+            createDayNames();
+            let days = "";
+            // 이전 달 날짜 표시
+            for (let i = firstdayIndex; i > 0; i--) {
+                days += `<div class='day prev-date'>${
+                    prevLastDay - i + 1
+                }</div>`;
+            }
 
-      for (let j = 1; j <= lastDay; j++) {
-        // 오늘인 경우
-        if (
-          j === new Date().getDate() &&
-                      date.getMonth() === new Date().getMonth() && date.getFullYear() === new Date().getFullYear()
-        ) {
-          if (new Date(year, month, j).getDay() === 0) {
-            days += `<div class="day today sunday">${j}</div>`
-          } else {
-            days += `<div class="day today">${j}</div>`
-          }
-        } else if (new Date(year, month, j).getDay() === 0) {
-          // 일요일인 경우
-          days += `<div class="day sunday">${j}</div>`
-        } else {
-          days += `<div class="day">${j}</div>`
-        }
-      }
-      // 다음 달 날짜 표시
-      for (let k = 1; k <= nextDays; k++) {
-        days += `<div class="day next-date">${k}</div>`
-      }
+            for (let j = 1; j <= lastDay; j++) {
+                // 오늘인 경우
+                if (
+                    j === new Date().getDate() &&
+                    date.getMonth() === new Date().getMonth() &&
+                    date.getFullYear() === new Date().getFullYear()
+                ) {
+                    if (new Date(year, month, j).getDay() === 0) {
+                        days += `<div class="day today sunday">${j}</div>`;
+                    } else {
+                        days += `<div class="day today">${j}</div>`;
+                    }
+                } else if (new Date(year, month, j).getDay() === 0) {
+                    // 일요일인 경우
+                    days += `<div class="day sunday">${j}</div>`;
+                } else {
+                    days += `<div class="day">${j}</div>`;
+                }
+            }
+            // 다음 달 날짜 표시
+            for (let k = 1; k <= nextDays; k++) {
+                days += `<div class="day next-date">${k}</div>`;
+            }
 
-      $days.innerHTML += days
-    }
-    showDates()
-  }
+            $days.innerHTML += days;
+        };
+        showDates();
+    };
 
-  return { date, renderCalendar }
+    return { date, renderCalendar };
 }
